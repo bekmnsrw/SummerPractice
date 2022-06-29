76b2bf9f9e0e7fa79c4154f50dfdcee4198ef802
Ilya Bekmansurov <ilya.bekmansurov@gmail.com>
2022-06-30 01:34:22 +0300

    Empty proj

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.task1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val submit = findViewById<Button>(R.id.submit)
+
+        submit.setOnClickListener {
+
+            val name = findViewById<EditText>(R.id.name).text.toString()
+            val height = findViewById<EditText>(R.id.height).text.toString().toInt()
+            val weight = findViewById<EditText>(R.id.weight).text.toString().toDouble()
+            val age = findViewById<EditText>(R.id.age).text.toString().toInt()
+
+            val view = findViewById<TextView>(R.id.result)
+
+            if (name == "" || name.length > 50 || height < 0 || height > 250 || weight < 0 || weight > 250.0 || age > 150 || age < 0) {
+                view.text = "Данные введены некорректно"
+            } else {
+                val formula = (name.length + height + weight.toInt() + age).toString()
+                view.text = formula.toInt().toString()
+            }
+        }
     }
 }
